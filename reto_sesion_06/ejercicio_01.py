cbab62148ea3eb91882f7ab7310d597f9acb7257
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-05-31 13:07:02 -0400

    valor de un booleano

---
+++ reto_sesion_06/ejercicio_01.py
@@ -0,0 +1,3 @@
+valor = 0.0000000001
+es_true = bool(valor)
+print(es_true)
---
+++ reto_sesion_06/ejercicio_03.py
@@ -0,0 +1,7 @@
+# Tarjeta y huella
+tarjeta = 1
+huella = 0
+
+# Puerta se abre si tarjeta XOR huella
+puerta = (tarjeta != huella)
+print(puerta)
