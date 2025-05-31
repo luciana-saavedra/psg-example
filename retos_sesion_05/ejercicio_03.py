b3f2395c27c4af0b73c14d4db47b85b2aebaa26c
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-05-31 10:43:44 -0400

    conversión segundos

--- retos_sesion_05/ejercicio_03.py
+++ retos_sesion_05/ejercicio_03.py
@@ -6,6 +6,6 @@ segundos%=(24*3600)
 horas=segundos//3600
 segundos%=3600
 minutos=segundos//60
-segundos%=60
+segundos%=60 
 
 print(f"{semanas} semanas {dias} días {horas} horas {minutos} minutos {segundos} segundos")
