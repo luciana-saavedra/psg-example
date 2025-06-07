ca98eb32dde7782d07d62d38fa44904fb040e582
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-06-07 14:48:24 -0400

    resta de números

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,5 @@
+num1=int(input("ingrese el primer número: "))
+num2=int(input("ingrese el segundo número: "))
+num3=int(input("ingrese el tercer número: "))
+resta=num1-num2-num3
+print("la resta de los números es:", resta)
