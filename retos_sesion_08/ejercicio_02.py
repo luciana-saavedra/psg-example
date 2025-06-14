c76c747366b912b69524c7cb4ab43cb6cb3883f9
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-06-14 14:50:00 -0400

    Letras y slice

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,12 @@
+letras = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+
+# Imprimir el primer elemento
+print("Primer elemento:", letras[0])
+# Imprimir el último elemento
+print("Último elemento:", letras[-1])
+# Slice del índice 3 al 5 (del 4to al 6to elemento)
+print("Slice del índice 3 al 5:", letras[3:6])
+# Slice del 5 al 9 con pasos de 3
+print("Slice del 5 al 9 con paso de 3:", letras[5:10:3])
+# Slice del 9 al 0 con pasos de -2
+print("Slice del 9 al 0 con paso de -2:", letras[9::-2])
