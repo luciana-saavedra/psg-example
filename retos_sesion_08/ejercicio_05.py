48f5eda9077d370ab3e4436788b57a5f42e82f0a
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-06-14 14:58:59 -0400

    Guía telefónica

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,10 @@
+contactos = (
+    ("Jhon", "591123456", "Villa Fátima"),
+    ("Jane", "591456324", "Obrajes"),
+    ("Dogg", "5912212241", "Miraflores"),
+    ("Kity", "591735159", "Achumani"),
+)
+
+
+for contacto in contactos:
+    print(f"Nombre: {contacto[0]}, Teléfono: {contacto[1]}, Dirección: {contacto[2]}")
