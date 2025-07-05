b37d6ed0c093844b08ad41a9c455b670b5f0f466
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-05 14:25:00 -0400

    Conjunto de prendas de ropa

---
+++ retos_sesion_10/ejercicio_02.py
@@ -0,0 +1,4 @@
+prendas_deportivas = ["Short", "Playera", "Sudadera", "Tenis", "Short", "Calcetines"]
+prendas_formales = ["Saco", "Corbata", "Pantalón de vestir", "Zapatos", "Calcetines"]
+conjunto_prendas = set(prendas_deportivas + prendas_formales)
+print(conjunto_prendas)
