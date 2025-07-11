b6047e961c264881b3f87529343b7474ed68b5f0
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-11 19:56:52 -0400

    Operador XNOR

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -1,4 +1,21 @@
+print("Operador XNOR")
+
+# True XNOR True → True
+a = True
+b = True
+print((a and b) or (not a and not b))  #True
+
+# True XNOR False → False
 a = True
 b = False
-xnor = (a == b)
-print(xnor) 
+print((a and b) or (not a and not b))  #False
+
+# False XNOR True → False
+a = False
+b = True
+print((a and b) or (not a and not b))  #False
+
+# False XNOR False → True
+a = False
+b = False
+print((a and b) or (not a and not b))  #True
