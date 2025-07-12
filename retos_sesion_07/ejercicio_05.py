b4c7ef8dd08c06e8015edc31cb76d1631d64ee4d
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-11 20:09:07 -0400

    ¿Es palíndromo?

--- retos_sesion_07/ejercicio_05.py
+++ retos_sesion_07/ejercicio_05.py
@@ -1,6 +1,9 @@
 cadena = input("Ingresa una frase: ")
-cadena_limpia = cadena.replace(" ", "").lower()
-if cadena_limpia == cadena_limpia[::-1]:
-    print("True")
-else:
-    print("False")
+
+#Quitamos espacios y convertimos todo a minúsculas
+procesado = cadena.replace(" ", "").lower()
+
+#Evaluamos si es palíndromo
+es_palindromo = procesado == procesado[::-1]
+
+print("¿Es palíndromo?", es_palindromo)
