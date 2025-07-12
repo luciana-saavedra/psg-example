83abbbe4f3e4acf936682c51632f7970e16971f5
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-11 20:01:47 -0400

    Sistema de seguridad

--- retos_sesion_06/ejercicio_03.py
+++ retos_sesion_06/ejercicio_03.py
@@ -1,5 +1,22 @@
-tarjeta = 1
-huella = 0
-#La puerta se abre si tarjeta XOR huella
-puerta = (tarjeta != huella)
-print(puerta)
+print("Tabla de verdad - Sistema de seguridad")
+print("Tarjeta | Huella  | Puerta")
+
+#La puerta solo se abre si se usa la tarjeta o la huella, pero no ambas al mismo tiempo
+
+tarjeta = True
+huella = True
+print(tarjeta, " |", huella, "  |", (tarjeta or huella) and not (tarjeta and huella))
+
+tarjeta = True
+huella = False
+print(tarjeta, " |", huella, " |", (tarjeta or huella) and not (tarjeta and huella))
+
+tarjeta = False
+huella = True
+print(tarjeta, "|", huella, "  |", (tarjeta or huella) and not (tarjeta and huella))
+
+tarjeta = False
+huella = False
+print(tarjeta, "|", huella, " |", (tarjeta or huella) and not (tarjeta and huella))
+
+print("Utilizaríamos el operador XOR")
