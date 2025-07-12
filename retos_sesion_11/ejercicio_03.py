88c743db78efba4119c1b3e1078242e80d3afbc1
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-11 20:27:34 -0400

    Diccionario con tuplas de animales

---
+++ retos_sesion_11/ejercicio_03.py
@@ -0,0 +1,18 @@
+tupla_especies = (('canino', '🐶'), ('felino', '🐱'), ('aves', ['🐦', '🦅']))
+
+#Convertimos tupla a diccionario
+diccionario_especies = dict(tupla_especies)
+
+#Obtenermos y eliminamos el valor de la clave 'aves'
+aves = diccionario_especies.pop('aves')
+print(f"Especies de aves eliminadas: {aves}")
+
+#Modificamos el valor de la clave 'felino'
+diccionario_especies['felino'] = '🐈'
+
+#Cambiamos la clave 'canino' por 'caninos' y su valor por lista
+diccionario_especies['caninos'] = ['🐶', '🐕']
+diccionario_especies.pop('canino')
+
+#Diccionario modificado
+print(f"Diccionario de especies modificado:\n{diccionario_especies}")
