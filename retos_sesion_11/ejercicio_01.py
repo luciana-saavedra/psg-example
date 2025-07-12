a35236f35cdd8ca16cef95442996a0932777dbec
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-11 20:21:48 -0400

    Información de animal marino

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,10 @@
+animal_marino = {
+    "especie": "Tiburón Blanco",
+    "habitat": "Océano Pacífico",
+    "dieta": "Peces y mamíferos marinos",
+    "estado_salud": "Bueno",
+    "edad": 8,
+    "responsables": {"Carlos", "María", "Luciana"}  #Responsables
+}
+
+print(f"Información del animal marino:\n{animal_marino}")
