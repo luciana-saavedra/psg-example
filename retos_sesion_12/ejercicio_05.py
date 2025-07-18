d7edbc9e7ea7b714f2e424398282071a55080e8c
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-18 17:40:24 -0400

    App de gestión de contactos

--- retos_sesion_12/ejercicio_05.py
+++ retos_sesion_12/ejercicio_05.py
@@ -1,8 +1,9 @@
 nombre = input("Nombre: ")
 telefono = input("Teléfono: ")
 
-# Verificamos que: el nombre no esté vacío, el número tenga 13 caracteres 
-# y comience con '+' y los demás sean números
+#Verificamos que: el nombre no esté vacío, el número tenga 12 caracteres 
+# (11 números incluyendo el código del país y 1 más por el "+" ) y 
+# comience con '+' y los demás sean números
 
 es_valido = nombre and len(telefono) == 12 and telefono[0] == '+' and telefono[1:].isdigit()
 
