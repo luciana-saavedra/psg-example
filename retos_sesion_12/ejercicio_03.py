2ede6131d86eee0a5b6be2674a08e2b556c504f1
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-18 17:15:22 -0400

    Autos en común

---
+++ retos_sesion_12/ejercicio_03.py
@@ -0,0 +1,8 @@
+jhon_autos = {'Ferrari', 'Lamborghini', 'Porsche', 'Bugatti', 'McLaren'}
+jess_autos = {'Ferrari', 'Lamborghini', 'Tesla', 'Ford', 'Chevrolet'}
+
+#Obtenemos la intersección de ambos conjuntos
+autos_comunes = jhon_autos & jess_autos
+
+#Mostramos los autos que tienen en común
+print("Autos que tienen en común Jhon y Jess:", autos_comunes)
