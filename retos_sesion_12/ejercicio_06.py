cce6c3c1be667b53829e66cc36af7ec5a2befd7d
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-07-19 11:42:54 -0400

    Calculadora

--- retos_sesion_12/ejercicio_06.py
+++ retos_sesion_12/ejercicio_06.py
@@ -1,6 +1,10 @@
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
-operacion = input("Ingresa la operación (+, -, *, /): ")
+entrada = input("Ingrese los datos en este formato: número1, número2, operación(+, -, *, /): ")
+
+#Separamos los tres valores usando split y quitamos espacios con strip
+num1_str, num2_str, operacion = entrada.split(",")
+num1 = float(num1_str.strip())
+num2 = float(num2_str.strip())
+operacion = operacion.strip()
 
 #Verificamos qué operación quiere hacer el usuario
 if operacion == "+":
@@ -13,10 +17,10 @@ elif operacion == "/":
     if num2 != 0:
         resultado = num1 / num2
     else:
-        resultado = "Error: División por cero"  #No se puede dividir entre 0
+        resultado = "Error: División por cero" #No se puede dividir entre 0
 else:
-    resultado = "Operación no válida"  #Porque la operación no está dentro de las que nuestra calculadora ofrece
+    resultado = "Operación no válida" #Porque la operación no está dentro de las que nuestra calculadora ofrece
 
-#Mostramos el resultado final
+#Mostramos el resultado
 print("-------------")
 print("Resultado:", resultado)
