b57b3cb365f4f50233cdd108fd999d20b9c1eae3
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-08-16 11:00:53 -0400

    Autos en común

--- retos_sesion_12/ejercicio_03.py
+++ retos_sesion_12/ejercicio_03.py
@@ -1,8 +1,12 @@
 jhon_autos = {'Ferrari', 'Lamborghini', 'Porsche', 'Bugatti', 'McLaren'}
 jess_autos = {'Ferrari', 'Lamborghini', 'Tesla', 'Ford', 'Chevrolet'}
 
-#Obtenemos la intersección de ambos conjuntos
+#Obtenemos los autos en común
 autos_comunes = jhon_autos & jess_autos
 
-#Mostramos los autos que tienen en común
-print("Autos que tienen en común Jhon y Jess:", autos_comunes)
+#Verificamos si existen autos en común
+if autos_comunes:
+    print("Sí, Jhon y Jess tienen autos en común.")
+    print("Autos en común:", autos_comunes)
+else:
+    print("No hay autos en común entre Jhon y Jess.")
