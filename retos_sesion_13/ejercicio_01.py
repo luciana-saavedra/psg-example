3fbd5ec31e66db4f21f53df2df37eed5474b484f
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-08-16 11:11:18 -0400

    Sucesión de Lucas

--- retos_sesion_13/ejercicio_01.py
+++ retos_sesion_13/ejercicio_01.py
@@ -1,7 +1,9 @@
 #Inicializamos los dos primeros términos de la sucesión de Lucas
 a, b = 2, 1
 print("Sucesión de Lucas:")
+
 #Usamos un ciclo for para imprimir los primeros 20 términos
 for i in range(20):
-    print(a, end=", ")
+    #Imprimimos los términos
+    print(a, end=", " if i < 19 else "\n")
     a, b = b, a + b
