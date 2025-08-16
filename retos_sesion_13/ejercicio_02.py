74c62af87f78c4336f282a64c26b5314d7090c06
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-08-16 11:14:02 -0400

    Múltiplos de 2 y 5

--- retos_sesion_13/ejercicio_02.py
+++ retos_sesion_13/ejercicio_02.py
@@ -1,10 +1,11 @@
-print("20 primeros múltiplos de 2 y 5: ")
-contador = 0  #Contador de números encontrados
+print("20 primeros múltiplos de 2 y 5:")
+
+contador = 0  #Contador de múltiplos encontrados
 numero = 1    #Número que vamos a evaluar
 
 while contador < 20:
-    # Verificamos si el número es divisible por 2 y 5
     if numero % 2 == 0 and numero % 5 == 0:
-        print(numero, end=", ")
+        #Imprimimos los múltiplos
+        print(numero, end=", " if contador < 19 else "\n")
         contador += 1
     numero += 1  #Pasamos al siguiente número
