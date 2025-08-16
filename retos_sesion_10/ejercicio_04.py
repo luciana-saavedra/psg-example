6cf1f9cd0fd2db486a6a88fc15c71c637453cd91
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-08-15 23:38:22 -0400

    Jane y Jhon

--- retos_sesion_10/ejercicio_04.py
+++ retos_sesion_10/ejercicio_04.py
@@ -1,8 +1,17 @@
 postres_jane = {"Lemon Pie", "Brownie", "Tarta de Manzana", "Helado de Chocolate", "Flan"}
 postres_jhon = {"Carrot Cake", "Croissant de Chocolate", "Lemon Pie", "Tarta de Manzana", "Pudding"}
 
+#Postres en común
 postres_en_comun = postres_jane & postres_jhon
-porcentaje = (len(postres_en_comun) / len(postres_jane)) * 100
 
-mensaje = "son compatibles" if porcentaje > 50 else "deben replantear su relación"
+#Porcentaje de postres
+total_postres = len(postres_jane | postres_jhon)
+porcentaje = (len(postres_en_comun) / total_postres) * 100
+
+#Mensaje basado en porcentaje
+if porcentaje > 50:
+    mensaje = "son compatibles"
+else:
+    mensaje = "deben replantear su relación"
+
 print(f"Jane y Jhon {mensaje}")
