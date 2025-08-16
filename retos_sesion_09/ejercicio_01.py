1f5fa5b1aa9d84db4271f819023e55e3f685c1dd
Luciana Saavedra <scarlet.saavedra.ayarde@gmail.com>
2025-08-15 22:47:09 -0400

    Números y tipo de dato

--- retos_sesion_09/ejercicio_01.py
+++ retos_sesion_09/ejercicio_01.py
@@ -1,6 +1,13 @@
-numeros = [3.14, 5.0, 2.718, 10, 15, 7, 21, 100, 42, 8.5]
+numeros = [3.14, 5.0, 2.718, 8.5, 7.1, 10, 15, 7, 21, 100]
 
-for indice in range(len(numeros)):
-    elemento = numeros[indice]
-    tipo_dato = type(elemento)
-    print(f"Elemento en el índice {indice}: {elemento}, tipo de dato: {tipo_dato}")
+#Mostramos cada elemento y su tipo de dato usando sus índices
+print("Elemento en el índice 0:", numeros[0], ", tipo de dato:", type(numeros[0]))
+print("Elemento en el índice 1:", numeros[1], ", tipo de dato:", type(numeros[1]))
+print("Elemento en el índice 2:", numeros[2], ", tipo de dato:", type(numeros[2]))
+print("Elemento en el índice 3:", numeros[3], ", tipo de dato:", type(numeros[3]))
+print("Elemento en el índice 4:", numeros[4], ", tipo de dato:", type(numeros[4]))
+print("Elemento en el índice 5:", numeros[5], ", tipo de dato:", type(numeros[5]))
+print("Elemento en el índice 6:", numeros[6], ", tipo de dato:", type(numeros[6]))
+print("Elemento en el índice 7:", numeros[7], ", tipo de dato:", type(numeros[7]))
+print("Elemento en el índice 8:", numeros[8], ", tipo de dato:", type(numeros[8]))
+print("Elemento en el índice 9:", numeros[9], ", tipo de dato:", type(numeros[9]))
